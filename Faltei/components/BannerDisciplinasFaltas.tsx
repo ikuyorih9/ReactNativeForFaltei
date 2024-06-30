279a392c95cc7e495dfc08cc1b6c76a2de42076f
Isaac Soares <isaacsantos.soares@hotmail.com>
2024-06-29 21:06:29 -0300

    Create BannerDisciplinasFaltas.tsx

---
+++ Faltei/components/BannerDisciplinasFaltas.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import  { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export function BannerDisciplinasFaltas() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Faltas</Text>
+        </View>
+    );
+}
