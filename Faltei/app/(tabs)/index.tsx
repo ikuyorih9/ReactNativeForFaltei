958ec42f98a457147864ea03bf5ad01d61be2dd6
Isaac Soares <isaacsantos.soares@hotmail.com>
2024-07-02 18:49:31 -0300

    Update index.tsx

--- Faltei/app/(tabs)/index.tsx
+++ Faltei/app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
     {value: 54, color: '#177AD5', text: '54%'},
     {value: 40, color: '#79D2DE', text: '30%'},
     {value: 20, color: '#ED6665', text: '26%'},
-];
+  ];
 
   return (
     <ThemedView>
